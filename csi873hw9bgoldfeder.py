1980c0a789f6a337795e35aab9130dc697fc9e2f
Bruce Goldfeder <bgoldfe2@masonlive.gmu.edu>
2017-11-14 17:45:40 -0500

    adding in the main and file reading

--- csi873hw9bgoldfeder.py
+++ csi873hw9bgoldfeder.py
@@ -11,6 +11,36 @@ Created on Mon Nov 13 22:27:25 2017
 import numpy as np
 import operator
 
+def ReadInFiles(path,trnORtst):
+    # This reads in all the files from a directory filtering on what the file
+    # starts with
+    fullData = []
+    fnames = os.listdir(path)
+    for fname in fnames:
+        if fname.startswith(trnORtst):
+            print (fname)
+            data = np.loadtxt(path + "\\" + fname)
+            fullData.append(data)
+    #numFiles = len (fullData)
+    #print(numFiles)
+   
+    return fullData
+    
+def ReadInOneList(fullData,maxRows):
+    # This function combines all of the data into one array for ease of use
+    # It contains a capping ability to configure how many results to use
+    allData = []
+    numFiles = len (fullData)
+    for j in range (numFiles):
+        # allows for smaller data set sizes
+        numRows = len (fullData[j])
+        #print('numrows,maxrows ',numRows,maxRows)
+        if (maxRows < numRows):
+            numRows = maxRows
+    
+        for k in range(numRows):
+            allData.append(fullData[j][k])
+    return np.asarray(allData)
 
 # Takes the x_q input to be tested, the training data array, the answer array,
 # and the value for k
@@ -49,3 +79,62 @@ def weight(sqDistances,epsilon,flag):
         weightList = np.divide(1,np.add(sqDistances,epsilon))
     
     return weightList
+
+def driver(dpath,trnNum,tstNum):
+    
+    # Read in the Training data first
+    datasetTrn = ReadInFiles(dpath,'train')
+    my_data = ReadInOneList(datasetTrn,trnNum)
+    
+    # Convert the 0-255 to 0 through 1 values in data
+    my_data[:,1:] /= 255.0
+    #HeatMap(my_data[40,1:])
+    
+    # randomize the rows for better training
+    np.random.shuffle(my_data)
+    inNum,cols = my_data.shape    
+    just_trn_data = my_data[:,1:]
+    answerTrn = my_data[:,0]
+    
+    # Read in the test data
+    #dpath2 = os.getcwd()+'\data3'
+    dataset2 = ReadInFiles(dpath,'test')
+    my_test = ReadInOneList(dataset2,tstNum) 
+    
+    tstNum,cols = my_test.shape
+    #print('num rows ',tstNum)
+    
+    # Convert the 0-255 to 0 through 1 values in data
+    my_test[:,1:] /= 255.0
+    
+    just_test_data = my_test[:,1:]
+    answerTest = my_test[:,0]    
+
+if __name__ == "__main__":
+    
+    from optparse import OptionParser
+
+    parser = OptionParser()
+    parser.add_option("-f", "--file", dest="filepath", help="Folder path for data")
+    parser.add_option("-t", "--train", dest="trnNum", help="Number of Training Images per Number")
+    parser.add_option("-x", "--test", dest="tstNum", help="Number of Test Images per Number")
+
+    options, args = parser.parse_args()
+    
+    if not options.filepath :
+        print("Used default of data" )
+        filepath = os.getcwd()+'\data'
+    else: filepath = options.filepath
+    
+    if not options.trnNum :
+        print("Used default trnNum = 1000" )
+        trnNum = 4500
+    else: trnNum = int(options.trnNum)
+    
+    if not options.tstNum :
+        print("Used default tstNum = 500" )
+        tstNum = 890
+    else: tstNum = int(options.tstNum) 
+    
+    # Call the driver function for KNN
+    driver()
